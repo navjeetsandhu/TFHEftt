eab4780a1578d8ccd15005a29b49321650f78e47
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-10-15 14:32:49 -0400

    changes

--- test/mult_fft_test.cpp
+++ test/mult_fft_test.cpp
@@ -1,31 +1,56 @@
 #include "mult_fft.hpp"
 #include"utils.hpp"
 
-#define N_FFT_TEST 16
 
-void test_mult_fft(const std::array<uint64_t, N_FFT_TEST>& p1, const std::array<uint64_t, N_FFT_TEST>& p2)
+template <int N>
+void test_mult_fft(const std::array<uint64_t, N>& p1, const std::array<uint64_t, N>& p2)
 {
 
-    std::string string_msg = "Naive Multiplication";
+    std::string string_msg = "Naive 64 bit Multiplication";
     print_results<uint64_t>("Input p1", p1.data(), p1.size());
     print_results<uint64_t>("Input p2", p2.data(), p1.size());
-    std::array<uint64_t, N_FFT_TEST> result = {0,0,0,0};
-    PolyMulNaive<uint64_t, N_FFT_TEST>(result, p1, p2);
+    std::array<uint64_t, N> result = {0,0,0,0};
+    PolyMulNaive<uint64_t, N>(result, p1, p2);
     print_results<int64_t>(string_msg,  reinterpret_cast<int64_t*>(result.data()), result.size());
 
 
     std::fill(result.begin(), result.end(), 0);
-    PolyMulFFT<uint64_t, N_FFT_TEST>(result, p1, p2);
-    string_msg = "FFT Multiplication";
+    PolyMulFFT<uint64_t, N>(result, p1, p2);
+    string_msg = "FFT 64 bit Multiplication";
     print_results<int64_t>(string_msg,  reinterpret_cast<int64_t*>(result.data()), result.size());
 }
 
+template <int N>
+void test_mult_fft(const std::array<uint32_t, N>& p1, const std::array<uint32_t, N>& p2)
+{
+
+    std::string string_msg = "Naive 32 bit Multiplication";
+    print_results<uint32_t>("Input p1", p1.data(), p1.size());
+    print_results<uint32_t>("Input p2", p2.data(), p1.size());
+    std::array<uint32_t, N> result = {0,0,0,0};
+    PolyMulNaive<uint32_t, N>(result, p1, p2);
+    print_results<int32_t>(string_msg,  reinterpret_cast<int32_t*>(result.data()), result.size());
+
+    std::fill(result.begin(), result.end(), 0);
+    PolyMulFFT<uint32_t, N>(result, p1, p2);
+    string_msg = "FFT 32 bit Multiplication";
+    print_results<int32_t>(string_msg,  reinterpret_cast<int32_t*>(result.data()), result.size());
+}
+
+
+template <class P, int N>
+void test_mult_fft()
+{
+    // make sure set N_FFT to 16 before testing
+    std::array<P,  N> p1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    std::array<P,  N> p2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 21, 22, 23, 24, 25, 26};
+    test_mult_fft<N>(p1, p2);
+}
 
 int main()
 {
     // make sure set N_FFT to 16 before testing
-    std::array<uint64_t,  N_FFT_TEST> p1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    std::array<uint64_t,  N_FFT_TEST> p2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 21, 22, 23, 24, 25, 26};
-    test_mult_fft(p1, p2);
+    test_mult_fft<uint32_t, N_FFT>();
+    test_mult_fft<uint64_t, N_FFT>();
     return 0;
 }

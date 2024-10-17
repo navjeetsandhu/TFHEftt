32f246a00aa9e8b87432bd805def5da0a03c6a2d
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-10-17 15:23:41 -0400

    changes

--- include/file.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <fstream>
-
-int  read_data_from_file(std::string file_name, std::vector<uint64_t>& data);
--- src/file.cpp
+++ src/file.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <vector>
-#include "file.h"
 #include <unistd.h>
-#include <limits.h>
 
 int print_current_directory() {
     char cwd[PATH_MAX];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    if (getcwd(cwd, sizeof(cwd)) != nullptr) {
         std::cout << "Current working dir: " << cwd << std::endl;
     } else {
         std::cerr << "getcwd() error" << std::endl;
@@ -15,24 +13,3 @@ int print_current_directory() {
     return 0;
 }
 
-int  read_data_from_file(std::string file_name, std::vector<uint64_t>& data) {
-    std::cout << std::endl << "Reading from  " << file_name << std::endl;
-
-    std::ifstream file(file_name);
-    if (!file)
-    {
-        print_current_directory();
-        std::cerr << "Unable to open " << file_name << std::endl;
-        return -1;
-    }
-
-    uint32_t value;
-    while (file >> value)
-    {
-        // Push each value into the vector
-        data.push_back(value);
-    }
-
-    file.close();
-    return 0;
-}

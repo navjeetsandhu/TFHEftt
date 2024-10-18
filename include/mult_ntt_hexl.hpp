2de99ddd5b6683cdba4c02b3fb6519d497294e2b
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-10-18 11:24:22 -0400

    msg

--- include/mult_ntt_hexl.hpp
+++ include/mult_ntt_hexl.hpp
@@ -11,20 +11,25 @@ namespace hexl {
     // 0x4000000000000211
     constexpr uint64_t moduli = 4611686018427365377ULL;
 
-
     inline void compute_inverse(uint64_t *result,
                                 const uint64_t *operand,
-                                intel::hexl::NTT& ntt);
+                                intel::hexl::NTT& ntt){
+        ntt.ComputeInverse(result, operand, 1, 1);
+    }
 
     inline void compute_forward(uint64_t *result,
                                 const uint64_t *operand,
-                                intel::hexl::NTT& ntt);
+                                intel::hexl::NTT& ntt) {
+        ntt.ComputeForward(result, operand, 1, 1);
+    }
 
     inline void eltwise_mult_mod(uint64_t *result,
                                  const uint64_t *p1,
                                  const uint64_t *p2,
                                  uint64_t degree,
-                                 uint64_t modulus) ;
+                                 uint64_t modulus) {
+        intel::hexl::EltwiseMultMod(result, p1, p2, degree, modulus,1);
+    }
 
     template<int N>
     inline void TwistNTT(std::array<uint64_t, N> &res,

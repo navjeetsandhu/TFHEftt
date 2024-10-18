155b90ad2b451f7234df884d9d209a8131f41ec1
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-10-18 12:32:56 -0400

    changes

--- test/ntt_test.cpp
+++ test/ntt_test.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
+#include <numeric>
 #include "mult_ntt_hexl.hpp"
+#include "mult_fft.hpp"
+#include"utils.hpp"
 
 
 std::unique_ptr<intel::hexl::NTT> create_ntt(uint64_t N, uint64_t modulus) {
@@ -19,3 +22,36 @@ void poly_mult_ntt(uint64_t *result, const uint64_t *p1, const uint64_t *p2,
 }
 
 
+template <int N>
+void test_mult(const std::array<uint64_t, N>& p1, const std::array<uint64_t, N>& p2) {
+
+    std::string string_msg = "Naive 64 bit Multiplication";
+    print_results<uint64_t>("Input p1", p1.data(), p1.size());
+    print_results<uint64_t>("Input p2", p2.data(), p1.size());
+    std::array<uint64_t, N> result = {};
+    PolyMulNaive<uint64_t, N>(result, p1, p2);
+    print_results<int64_t>(string_msg, reinterpret_cast<int64_t *>(result.data()), result.size());
+
+    std::fill(result.begin(), result.end(), 0);
+    hexl::PolyMulNTT<N>(result, p1, p2);
+    string_msg = "hexl NTT 64 bit Multiplication";
+    print_results<int64_t>(string_msg,  reinterpret_cast<int64_t*>(result.data()), result.size());
+}
+
+
+template<int N>
+void test_mult()
+{
+    std::array<uint64_t,  N> p1{};
+    std::array<uint64_t,  N> p2{};
+    std::iota(p1.begin(), p1.end(), 1);
+    std::iota(p2.begin(), p2.end(), 10);
+    test_mult<N>(p1, p2);
+}
+
+int main()
+{
+
+    test_mult<1<<6>();
+    return 0;
+}
